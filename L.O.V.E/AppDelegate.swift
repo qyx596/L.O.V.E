f3b1b936e60fe07f2e69630fd40ff864748d74eb
qyx596 <yuxuanqiu596@gmail.com>
2021-01-28 11:43:47 +0800

    add notifications

--- L.O.V.E/AppDelegate.swift
+++ L.O.V.E/AppDelegate.swift
@@ -8,6 +8,7 @@
 import Cocoa
 import Foundation
 import SwiftyTimer
+import UserNotifications
 @NSApplicationMain
 
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -20,21 +21,48 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let date = Date()
     let dateFormatter = DateFormatter()
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    let content = UNMutableNotificationContent()
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+    let hour = Calendar.current.component(.hour, from: Date())
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         statusItem.menu = statusMenu
         dateFormatter.dateFormat = "YYYYMMdd"
         let love = dateFormatter.date(from: "20201027")
+        UNUserNotificationCenter
+            .current()
+            .requestAuthorization(options: [.alert, .badge, .sound], completionHandler:  { (success, error) in
+                print("授权" + (success ? "成功" : "失败"))
+            })
+        content.title = "程序状态"
+        content.subtitle = "启动成功"
+        content.sound = UNNotificationSound.default
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
         Timer.every(1.seconds) {
             let todate = self.dateFormatter.string(from: self.date).suffix(4)
             let betweendays = Calendar.current.dateComponents([.day], from: love!, to: Date())
             let days = Int(betweendays.day!)
             self.statusItem.button?.title=String(days) + " " + NSLocalizedString("Days", comment:"")
-            switch (todate, days % 365) {
-            case ("1127", _), ("1128", _) :
+            switch (todate, days % 365, days % 100) {
+            case ("1127", _, _), ("1128", _, _) :
+                if (self.hour == 9) {
+                self.content.title = "重要提醒"
+                self.content.subtitle = "猪猪的生日"
+                self.content.sound = UNNotificationSound.default
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: self.content, trigger: self.trigger)
+                UNUserNotificationCenter.current().add(request)
+                }
                 self.statusItem.button!.image = NSImage(named: "birthdaycake")
-            case (_, 0) :
+            case (_, 0, _), (_, _, 0) :
+                if (self.hour == 9) {
+                self.content.title = "重要提醒"
+                self.content.subtitle = "重要日子"
+                self.content.sound = UNNotificationSound.default
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: self.content, trigger: self.trigger)
+                UNUserNotificationCenter.current().add(request)
                 self.statusItem.button!.image = NSImage(named: "pinkheart")
+                }
             default :
                 self.statusItem.button!.image = NSImage(named: "heart")
             }
---
+++ L.O.V.E/functions.swift
@@ -0,0 +1,12 @@
+//
+//  functions.swift
+//  L.O.V.E
+//
+//  Created by Yuxuan Qiu on 2021/1/28.
+//  Copyright © 2021 Yuxuan Qiu. All rights reserved.
+//
+
+import Foundation
+class test {
+    var test = "asshole"
+}
